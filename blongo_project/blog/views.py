7fdda45c5b52ddb3e2c73b0d31772a657dd8f641
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2019-05-25 21:09:34 +0530

    Add database and migrations

--- blongo_project/blog/views.py
+++ blongo_project/blog/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 23, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 10, 2019'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
