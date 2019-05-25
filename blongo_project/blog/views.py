f2f423cfdb92acbbd67d1409c707b7e024bc3bb0
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2019-05-25 17:51:53 +0530

    Add template, template hierarchy and styling

--- blongo_project/blog/views.py
+++ blongo_project/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 23, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 10, 2019'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', context={ 'title': 'About' })
